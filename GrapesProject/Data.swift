483b87ada94c6281c58e77542188705f965c27fb
Leonardo Catello <leonardo.catello23@gmail.com>
2024-03-07 17:17:58 +0100

    adding epub

--- GrapesProject/Data.swift
+++ GrapesProject/Data.swift
@@ -8,9 +8,12 @@
 import Foundation
 import SwiftUI
 import Observation
+import EPUBKit
 
 @Observable
 class SharedData {
+    let path = Bundle.main.url(forResource: "frankenstein", withExtension: "epub"),
+        let document = EPUBDocument(url: path)
     var Books = [
         book(title: "A Game of Thrones", cover: "got"),
         book(title: "The Lord of the Rings", cover: "lotr"),
