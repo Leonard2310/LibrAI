69c1e9ceef8aa2b779ab094f84fda224d02ad435
dragonsack <alepal2002@gmail.com>
2024-03-08 17:00:20 +0100

    wewe

--- GrapesProject/CollectablesView.swift
+++ GrapesProject/CollectablesView.swift
@@ -11,7 +11,7 @@ struct CollectablesView: View {
     var body: some View {
         
         //-----Insert content collectable
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hewewello, World!")
         //--------------
         
     }
