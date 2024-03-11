922baaf3fdcf04c119f0e0b51d76dfd3f6672f08
chiacor <148192586+chiacor@users.noreply.github.com>
2024-03-11 16:43:35 +0100

    Collectables View

--- GrapesProject/CollectablesView.swift
+++ GrapesProject/CollectablesView.swift
@@ -8,11 +8,77 @@
 import SwiftUI
 
 struct CollectablesView: View {
+    
+    var myData = SharedData()
+    
     var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Text("Last Earned")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.leading, 11.0)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack{
+                        ForEach(myData.Mice1) {
+                            Mouse in
+                            VStack{
+                                Image(Mouse.mouseImage)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .mask(Circle())
+                                    .frame(width: 130, height: 130)
+                                    .padding(.leading, 11.0)
+                                    .shadow(radius: 1)
+                                Text(Mouse.mouseName)
+                                    .padding(.leading, 11.0)
+                            }
+                        }
+                    }
+                }
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack{
+                        ForEach(myData.Mice2) {
+                            Mouse in
+                            VStack{
+                                Image(Mouse.mouseImage)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .mask(Circle())
+                                    .frame(width: 130, height: 130)
+                                    .padding(.leading, 11.0)
+                                Text(Mouse.mouseName)
+                                    .padding(.leading, 11.0)
+                            }
+                        }
+                    }
+                }
+                Text("App Challenges")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.leading, 11.0)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack{
+                        ForEach(myData.ChallengeMice) {
+                            Mouse in
+                            VStack{
+                                Image(Mouse.mouseImage)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .mask(Circle())
+                                    .frame(width: 130, height: 130)
+                                    .padding(.leading, 11.0)
+                                Text(Mouse.mouseName)
+                                    .padding(.leading, 11.0)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Collectables")
+        }
         
-        //-----Insert content collectable
-        Text("Collectables")
-        //--------------
+        
         
     }
 }
