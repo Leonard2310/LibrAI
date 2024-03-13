5a56423f2707887d48cc781bac4e8c71d5bfbf93
Salvr28 <salvatorecangiano14@gmail.com>
2024-03-13 17:05:46 +0100

    Text fix with rectangle view

--- GrapesProject/ImmersiveReadingView.swift
+++ GrapesProject/ImmersiveReadingView.swift
@@ -26,15 +26,11 @@ struct ImmersiveReadingView: View {
                         .fill(Color.readingRectangle)
                         .frame(width: 337.0
                                , height: RectReadingHeigh)
-                        
-                        
-                         
-                        Text("The morning had dawned clear and cold, with a crispness that hinted at the end of summer.\nThey set forth at daybreak to see a man beheaded, twenty in all, and Bran rode among them, nervous with excitement.\nThis was the first time he had been deemed old enough to go with his lord father and his brothers to see the king’s justice done.")
+                        .overlay(Text("The morning had dawned clear and cold, with a crispness that hinted at the end of summer.\nThey set forth at daybreak to see a man beheaded, twenty in all, and Bran rode among them, nervous with excitement.\nThis was the first time he had been deemed old enough to go with his lord father and his brothers to see the king’s justice done.")
                             .foregroundColor(.white)
                             .padding()
-                            .lineLimit(nil)
-                            .frame(width: 337.0, height: 263.0)
-                            
+                            .lineLimit(nil))
+
                     
                     Rectangle()
                         .fill(Color.white) // Customize the color as needed
